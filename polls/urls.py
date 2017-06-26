436a47fd2d438ac4fb9d27ec7bf777a9728ded42
Anthony <antho.tedde@gmail.com>
2017-06-26 10:37:29 +0200

    End of tutorial part 3

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+# app_name plays role of namespace.
+# It serves to differentiate url inside views.
+app_name = 'polls'
+
 urlpatterns =[
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
